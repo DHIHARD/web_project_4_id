852fbe2085acc18fab75b3b72a169bc3fdf4280f
DHIHARD <oldhihp@gmail.com>
2023-04-20 23:03:58 +0900

    hotflix/revisi-html, css, js 5

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,11 +4,11 @@ let editButton = content.querySelector('.profile__button-ubah');
 let profileName = content.querySelector('.profile__name');
 let profileAbout = content.querySelector('.profile__about');
 
-let formElement = document.querySelector('.popup');
+let popupSection = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__form');
 let closeButton = document.querySelector('.popup__button-close');
-let nameInput = document.querySelector('.popup__input_name-field');
-let aboutInput = document.querySelector('.popup__input_about-field');
+let nameInput = document.querySelector('#name');
+let aboutInput = document.querySelector('#about');
 
 let newName;
 let newAbout;
@@ -16,11 +16,11 @@ let newAbout;
 function popupVisible () {
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
-    formElement.classList.toggle('popup_opened')
+    popupSection.classList.toggle('popup_opened')
 }
 
 function popupClose () {
-    formElement.classList.toggle('popup_opened')   
+    popupSection.classList.toggle('popup_opened')   
 }
 
 function handleProfileFormSubmit (evt) {
@@ -39,6 +39,6 @@ editButton.addEventListener('click', popupVisible);
 
 closeButton.addEventListener('click', popupClose);
 
-formElement.addEventListener('submit', handleProfileFormSubmit);
+popupForm.addEventListener('submit', handleProfileFormSubmit);
 
 // tombol suka
